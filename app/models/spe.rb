bbccc1e8d2321e33fe91f96b507564a8c5aeb6bb
David ROBIN <david.robin97@gmail.com>
2017-07-09 21:42:36 +0200

    Add option to retrieve only specific questions for each spe

--- app/models/spe.rb
+++ app/models/spe.rb
@@ -13,4 +13,50 @@ class Spe < ActiveRecord::Base
     connection_log.each { |c| s += c.updated_at - c.created_at }
     s
   end
+
+  def unanswered_questions
+    questions_not_matching_query(answered_questions_query)
+  end
+
+  def unvoted_questions
+    questions_not_matching_query(voted_questions_query)
+  end
+
+  def unrated_questions
+    questions_not_matching_query(rated_questions_query)
+  end
+
+  private
+  def questions_not_matching_query(query)
+    all = Question.all.select(:id).map { |i| i[:id] }
+    matching = ActiveRecord::Base.connection.execute(query).map {|r| r["id"].to_i }
+    (all - matching).map { |i| Question.find i }
+  end
+
+  def answered_questions_query
+    <<-SQL
+      SELECT q.id FROM questions q
+        JOIN answers a ON a.question_id = q.id
+        JOIN answer_points ap ON ap.answer_id = a.id
+      WHERE ap.spe_id = #{id}
+      GROUP BY q.id
+      HAVING SUM(score) = 10
+    SQL
+  end
+
+  def voted_questions_query
+    <<-SQL
+      SELECT q.id FROM questions q
+        JOIN votes v ON v.question_id = q.id
+      WHERE v.spe_id = #{id} AND v.vote <> 0
+    SQL
+  end
+
+  def rated_questions_query
+    <<-SQL
+      SELECT q.id FROM questions q
+        JOIN suggested_coeffs c ON c.question_id = q.id
+      WHERE c.spe_id = #{id} AND c.coeff <> 0
+    SQL
+  end
 end
